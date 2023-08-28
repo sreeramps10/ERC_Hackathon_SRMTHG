708c6dfe7ccf6c512ba5a3d4302ffe0f252943e2
sreeramps10 <114275106+sreeramps10@users.noreply.github.com>
2023-08-28 10:34:19 +0530

    Update colour_detection.py

--- robotics_hackathon_automation/src/colour_detection.py
+++ robotics_hackathon_automation/src/colour_detection.py
@@ -1,6 +1,3 @@
-#!/usr/bin/env python3
-#You need to name this node "color_detector"
-
 import rospy
 import cv2
 from cv_bridge import CvBridge
@@ -18,35 +15,27 @@ class ColorDetectionNode:
         rospy.spin()
 
     def image_callback(self, msg):
-        # Convert ROS image to OpenCV format
         cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
-
-        # Detect colors and publish task status
         color_detected = self.detect_color(cv_image)
         self.publish_task_status(color_detected)
 
     def detect_color(self, image):
-        # Convert the image to the HSV color space
         hsv_image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        # Define color ranges for different colored cones (You may need to adjust these values)
         blue_lower = (90, 100, 100)
         blue_upper = (120, 255, 255)
         red_lower = (0, 100, 100)
         red_upper = (10, 255, 255)
 
-        # Threshold the image to extract specific colors
         mask_blue = cv2.inRange(hsv_image, blue_lower, blue_upper)
         mask_red = cv2.inRange(hsv_image, red_lower, red_upper)
 
-        # Count the number of blue and red pixels
         blue_pixel_count = cv2.countNonZero(mask_blue)
         red_pixel_count = cv2.countNonZero(mask_red)
 
-        # Determine the detected color based on the pixel count threshold (You may need to adjust these values)
-        if blue_pixel_count > 500:  # Adjust the threshold as per your requirement
+        if blue_pixel_count > 500:  
             color_detected = "Blue"
-        elif red_pixel_count > 500:  # Adjust the threshold as per your requirement
+        elif red_pixel_count > 500:  
             color_detected = "Red"
         else:
             color_detected = "Unknown"
